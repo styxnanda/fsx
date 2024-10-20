341d1cb7f6f1b9cf80742c81e460ad8acbd1a4ed
Satya Ananda <dev@satyaand.com>
2024-10-20 21:04:24 +0700

    Delete extension directory
    Due: Migration to Plasmo from WXT.

--- extension/wxt.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "wxt";
-
-// See https://wxt.dev/api/config.html
-export default defineConfig({
-  srcDir: "src",
-  extensionApi: "chrome",
-  modules: ["@wxt-dev/module-react"],
-});
